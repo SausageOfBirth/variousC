f7fcd22c98179a96179eb66a6370ac51f364947f
SausageOfBirth <mzn719@alumni.ku.dk>
2016-09-10 15:16:13 +0200

    concat working

--- main.c
+++ main.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char* numbers[31] = {"one","two","three","four","five","six","seven","eight","nine","ten",
                      "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen",
                      "eighteen","nineteen","twenty","thirty","fourty","fifty","sixty",
                      "seventy","eighty","ninety","hundred","thousand","million","and"};
 
+char* concat(char* str1,char* str2){
+  char* res = malloc((sizeof(char)*strnlen(str1,32))+(strnlen(str2,32) * sizeof(char)));
+  memcpy(res,str1,sizeof(char) * strnlen(str1,32));
+  memcpy(res+strnlen(str1,32)*sizeof(char),str2,sizeof(char)*strnlen(str2,32));
+
+  return res;
+}
+
 char* intToString(int num){
   char* res = "";
   if(num < 10){
-    char* resBuff = malloc(sizeof(char)*(sizeof(res)+sizeof(numbers[num])));
-    for(int n = 0; n < sizeof(res)+sizeof(numbers[num]);n++){
-      if(n < sizeof(res)){
-        resBuff[n] = res[n];
-      }else{
-        resBuff[n] = numbers[num][n-sizeof(res)];
-      }
-    }
+    res = concat(res,numbers[num]);
   }
 
   return res;
