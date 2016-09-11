d993ab2cfb05f9e879b65d557a340c611816c48c
SausageOfBirth <mzn719@alumni.ku.dk>
2016-09-11 19:06:24 +0200

    strange bug when giving 130, something over 100000 and ending in a 0 for the thousands

--- main.c
+++ main.c
@@ -2,29 +2,58 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* numbers[31] = {"one","two","three","four","five","six","seven","eight","nine","ten",
-                     "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen",
+char* numbers[32] = {" zero"," one"," two"," three"," four"," five"," six"," seven"," eight"," nine",
+                     "ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen",
                      "eighteen","nineteen","twenty","thirty","fourty","fifty","sixty",
-                     "seventy","eighty","ninety","hundred","thousand","million","and"};
+                     "seventy","eighty","ninety","hundred","thousand","million"," and "};
+
+int maxLen = 1024;
 
 char* concat(char* str1,char* str2){
-  char* res = malloc((sizeof(char)*strnlen(str1,32))+(strnlen(str2,32) * sizeof(char)));
-  memcpy(res,str1,sizeof(char) * strnlen(str1,32));
-  memcpy(res+strnlen(str1,32)*sizeof(char),str2,sizeof(char)*strnlen(str2,32));
+  char* res = malloc((sizeof(char)*strnlen(str1,maxLen))+(strnlen(str2,maxLen) * sizeof(char)));
+  memcpy(res,str1,sizeof(char) * strnlen(str1,maxLen));
+  memcpy(res+strnlen(str1,maxLen)*sizeof(char),str2,sizeof(char)*strnlen(str2,maxLen));
 
   return res;
 }
 
 char* intToString(int num){
   char* res = "";
-  if(num < 10){
+  char* tmp = "";
+  if(num < 21){
     res = concat(res,numbers[num]);
+  }else if(num < 100){
+    if((num % ((num/10)*10)) != 0){
+      res = concat((concat(res,numbers[(num/10)+18])),intToString(num-((num/10)*10)));
+    }else{
+      res = concat(res,numbers[(num/10)+18]);
+    }
+  }else if(num < 1000){
+    if((num % ((num/100)*100)) != 0){
+      tmp = concat(res,intToString((num/100)*100));
+      tmp = concat(tmp, numbers[31]);
+      res = concat(tmp,intToString(num-((num/100)*100)));
+    }else{
+      tmp = concat(res,numbers[(num/100)]);
+      tmp = concat(tmp," ");
+      res = concat(tmp,numbers[28]);
+    }
+  }else{
+    if((num % ((num/1000)*1000)) != 0){
+      tmp = concat(res,intToString((num/1000)*1000));
+      res = concat(tmp,intToString(num-((num/1000)*1000)));
+    }else{
+      tmp = concat(res,intToString(num/1000));
+      tmp = concat(tmp," ");
+      res = concat(tmp,numbers[29]);
+    }
   }
 
   return res;
 }
 
 int main(){
-  printf("%s\n",intToString(0));
+  printf("%s\n",intToString(120456));
+  printf("%i\n",33 % 30);
   return 0;
 }
